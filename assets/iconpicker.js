a2c95b05486130da12b728c66ab8a0d014159712
Alex Platter <a.platter@kreatif.it>
2017-10-13 12:11:13 +0200

    Bug fix
    Handling für Mehrfachinitialisierung

--- assets/iconpicker.js
+++ assets/iconpicker.js
@@ -2,8 +2,8 @@ $(document).on('ready pjax:success mblock:add',function() {
     $('.icp').each(function() {
         var $this = $(this);
 
-        if (!$this.parent().hasClass('iconpicker-container')) {
-            if ($this.hasClass('icp-group')) {
+        if (typeof $this.data('iconpicker') == 'undefined') {
+            if ($this.hasClass('icp-group') && !$this.next().hasClass('input-group-addon')) {
                 $this.wrap('<div class="input-group"/>').after('<span class="input-group-addon"/>');
             }
             $this.iconpicker();
